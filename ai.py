e7b98058709f6a294da4b7085e936754a451d757
danpeczek <danpeczek@gmail.com>
2021-08-10 17:47:34 +0200

    forward function

--- ai.py
+++ ai.py
@@ -6,7 +6,6 @@ from torch.autograd import Variable
 
 import gym
 
-
 # Building the AI
 
 def get_gpu():
@@ -28,7 +27,7 @@ class CNN(nn.Module):
         self.convolution1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=5)
         self.convolution2 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3)
         self.convolution3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=2)
-        self.fc1 = nn.Linear(in_features=self.count_neurons((1,80,80)), out_features=40)
+        self.fc1 = nn.Linear(in_features=self.count_neurons((1, 80, 80)), out_features=40)
         self.fc2 = nn.Linear(in_features=40, out_features=number_actions)
 
     def count_neurons(self, image_dim):
@@ -42,5 +41,13 @@ class CNN(nn.Module):
         x = F.relu(F.max_pool2d(self.convolution3(x), 3, 2))
         # Get neurons from all the channels and put them into the one vector
         return x.data.view(1, -1).size(1)
+
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.convolution1(x), 3, 2))
+        x = F.relu(F.max_pool2d(self.convolution2(x), 3, 2))
+        x = F.relu(F.max_pool2d(self.convolution3(x), 3, 2))
+        x = x.view(x.size(0), -1)
+        x = F.relu(self.fc1(x))
+        return self.fc2(x)
 
 # Deep Q-Learning implementation
