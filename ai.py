2b39a19caf48c08516a84733b8c9bc77b76fa8a7
danpeczek <danpeczek@gmail.com>
2021-08-10 17:28:22 +0200

    Count neurons

--- ai.py
+++ ai.py
@@ -6,6 +6,7 @@ from torch.autograd import Variable
 
 import gym
 
+
 # Building the AI
 
 def get_gpu():
@@ -14,7 +15,6 @@ def get_gpu():
         return 1
     print("*****NO CUDA")
     return -1
-
 
 
 class CNN(nn.Module):
@@ -28,8 +28,19 @@ class CNN(nn.Module):
         self.convolution1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=5)
         self.convolution2 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3)
         self.convolution3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=2)
-        self.fc1 = nn.Linear(in_features=number_neurons, out_features=40)
+        self.fc1 = nn.Linear(in_features=self.count_neurons((1,80,80)), out_features=40)
         self.fc2 = nn.Linear(in_features=40, out_features=number_actions)
 
+    def count_neurons(self, image_dim):
+        # 1- batch, image_dim - dimensions of the image - channels, width, height
+        # * - to pass tuple argument as a list of arguments to a function
+        x = Variable(torch.rand(1, *image_dim))
+        # 3 - kernel size
+        # 2 - stride - how many pixels it's going to slide into the images
+        x = F.relu(F.max_pool2d(self.convolution1(x), 3, 2))
+        x = F.relu(F.max_pool2d(self.convolution2(x), 3, 2))
+        x = F.relu(F.max_pool2d(self.convolution3(x), 3, 2))
+        # Get neurons from all the channels and put them into the one vector
+        return x.data.view(1, -1).size(1)
 
 # Deep Q-Learning implementation
