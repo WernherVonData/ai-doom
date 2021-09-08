6ff54010ceffd364c00c80a25b853f7ce0c3f2ea
Daniel Pęczek <danpeczek@gmail.com>
2021-09-08 16:05:02 +0200

    Issue 2 use vizdoom with eligibility trace and cnn (#3)
    Right now the implementation works - probably there are some issues inside my implementation, need to check it with the colab notebook.

--- image_preprocessing.py
+++ image_preprocessing.py
@@ -1,28 +1,13 @@
-# Image Preprocessing
-
-# Importing the libraries
+import cv2
 import numpy as np
-from scipy.misc import imresize
-from gym.core import ObservationWrapper
-from gym.spaces.box import Box
 
-# Preprocessing the Images
 
-class PreprocessImage(ObservationWrapper):
-    
-    def __init__(self, env, height = 64, width = 64, grayscale = True, crop = lambda img: img):
-        super(PreprocessImage, self).__init__(env)
-        self.img_size = (height, width)
-        self.grayscale = grayscale
-        self.crop = crop
-        n_colors = 1 if self.grayscale else 3
-        self.observation_space = Box(0.0, 1.0, [n_colors, height, width])
-
-    def _observation(self, img):
-        img = self.crop(img)
-        img = imresize(img, self.img_size)
-        if self.grayscale:
-            img = img.mean(-1, keepdims = True)
-        img = np.transpose(img, (2, 0, 1))
-        img = img.astype('float32') / 255.
-        return img
+def process_image_to_grayscale(image, width = 64, height = 64):
+    img = image.copy()
+    # First axis of doom screen buffer is the number of channels, while the OpenCV takes channels as the last parameter.
+    img = np.moveaxis(img, 0, -1)
+    img = cv2.cvtColor(img, cv2.COLOR_RGBA2GRAY)
+    img = cv2.resize(img, dsize=(height, width), interpolation=cv2.INTER_CUBIC)
+    # Need to reshape again to get it in the way (1, height, width)
+    img = img.reshape(1, img.shape[0], img.shape[1])
+    return img
