eb414710867db2e23ac1d26e954c2fb192d25160
danpeczek <danpeczek@gmail.com>
2021-09-14 15:43:45 +0200

    softmax body, save, load, softmax_body from other scripts used

--- agent_player.py
+++ agent_player.py
@@ -1,25 +1,12 @@
 import vizdoom as vzd
-import os
 import torch
 from torch.autograd import Variable
-import torch.nn as nn
-import torch.nn.functional as F
 from agents import cnn_agent
 import image_preprocessing
 import numpy as np
 from time import sleep
-
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-
-
-def load(agent_file, model_used):
-    if os.path.isfile(agent_file):
-        print("=>loading agent")
-        agent = torch.load(agent_file)
-        model_used.load_state_dict(agent['state_dict'])
-    else:
-        print("no checkpoint found...")
-    return model_used
+import utils
+from softmax_body import SoftmaxBody
 
 
 class AI:
@@ -28,28 +15,15 @@ class AI:
         self.body = body
 
     def __call__(self, inputs):
-        input_images = Variable(torch.from_numpy(np.array(inputs, dtype=np.float32))).to(device)
+        input_images = Variable(torch.from_numpy(np.array(inputs, dtype=np.float32))).to(utils.DEVICE_NAME)
         output = self.brain(input_images)
         actions = self.body(output)
         return actions.data.cpu().numpy()
 
 
-class SoftmaxBody(nn.Module):
-
-    def __init__(self, temperature):
-        super(SoftmaxBody, self).__init__()
-        self.temperature = temperature
-
-    # Outputs from the neural network
-    def forward(self, outputs):
-        probabilities = F.softmax(outputs * self.temperature, dim=len(outputs))
-        actions = probabilities.multinomial(num_samples=1)
-        return actions
-
-
 if __name__ == '__main__':
     scenario = "scenarios/basic.cfg"
-    print("=>device: {}".format(device))
+    print("=>device used: {}".format(utils.DEVICE_NAME))
 
     actions = []
     nb_available_buttons = 3
@@ -59,8 +33,8 @@ if __name__ == '__main__':
     image_dim = 128
 
     cnn = cnn_agent.CNN(number_actions=nb_available_buttons, image_dim=image_dim)
-    cnn = load("experiments\\basic_scenario\\basic_cnn_doom_50.pth", cnn)
-    cnn.to(device)
+    cnn = utils.load("experiments\\basic_scenario\\basic_cnn_doom_50.pth", cnn)
+    cnn.to(utils.DEVICE_NAME)
     softmax_body = SoftmaxBody(temperature=1.0)
     ai = AI(brain=cnn, body=softmax_body)
     game = vzd.DoomGame()
--- utils.py
+++ utils.py
@@ -1,7 +1,8 @@
 import torch
 import numpy as np
+import os
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
+DEVICE_NAME = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
 class MemoryAverage:
@@ -19,3 +20,18 @@ class MemoryAverage:
 
     def average(self):
         return np.mean(self.list_of_rewards)
+
+
+def save(filename, model, optimizer):
+    torch.save({'state_dict': model.state_dict(),
+                'optimizer': optimizer.state_dict()}, filename)
+
+
+def load(agent_file, model_used):
+    if os.path.isfile(agent_file):
+        print("=>loading agent")
+        agent = torch.load(agent_file)
+        model_used.load_state_dict(agent['state_dict'])
+    else:
+        print("no checkpoint found...")
+    return model_used
