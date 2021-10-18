d772482486180267c8bf90b6c04d8ee76b9c1905
danpeczek <danpeczek@gmail.com>
2021-10-18 22:04:19 +0200

    instead of injecting functionalities, the agent trainer relies on the abstract agent object

--- agent_trainer.py
+++ agent_trainer.py
@@ -1,69 +1,38 @@
-from collections import deque, namedtuple
+from collections import deque
 import datetime
 from random import choice
 
 import matplotlib.pyplot as plt
-import torch.nn as nn
-import torch.optim as optim
+
 from torch.autograd import Variable
 
 import vizdoom as viz
 
 import utils
-from agents import cnn_agent, cnn_agent_second_input
-from katie.rl import SoftmaxBody
+
 from katie.rl import ReplayMemory
 from utils import MemoryAverage
 
 
 class AgentTrainer:
-    def __init__(self, agent_name, scenario_name, temperature=1.0, memory_capacity=100000, lr=0.001):
-        self.scenario_path, self.nb_available_buttons = utils.get_path_and_number_of_actions_to_scenario(
-            scenario_name=scenario_name)
-        self.actions = []
-        for i in range(0, self.nb_available_buttons):
-            self.actions.append(
-                [True if action_index == i else False for action_index in range(0, self.nb_available_buttons)])
-
-        self.image_dim = 80
-        self.cnn = None
-        self.ai = None
-        self.softmax_body = SoftmaxBody(temperature=temperature)
-        if agent_name == "cnn_image":
-            self.cnn = cnn_agent.CNN(number_actions=self.nb_available_buttons, image_dim=image_dim)
-            self.ai = cnn_agent.AI(brain=self.cnn, body=self.softmax_body)
-        if agent_name == "cnn_image_linear":
-            self.cnn = cnn_agent_second_input.CnnTwoInput(number_actions=self.nb_available_buttons, image_dim=image_dim,
-                                                          linear_input=4)
-
-        if self.cnn is None or self.ai is None:
-            raise ValueError("Agent not instantiated properly")
-
-        self.cnn.to(utils.DEVICE_NAME)
+    def __init__(self, agent_to_train, memory_capacity=100000):
+        self.agent = agent_to_train
         self.ma = MemoryAverage(100)
-        self.loss = nn.MSELoss()
-        self.optimizer = optim.Adam(self.cnn.parameters(), lr=lr)
         self.memory = ReplayMemory(capacity=memory_capacity)
 
-    def load_agent_optimizer_and_replay_memory(self, model_path, memory_path):
-        self.cnn, self.optimizer = utils.load(model_path, model_used=self.cnn, optimizer_used=self.optimizer)
-        self.memory.load_memory_buffer(memory_path)
-
-    def train(self, step_reader, make_action, calculate_reward, generate_history, eligibility_trace, starting_epoch=1,
-              nb_epochs=100, batch_size=128, n_step=10, nb_steps=1000, gamma=0.99):
+    def train(self, starting_epoch=1, nb_epochs=100, batch_size=128, n_step=10, nb_steps=1000, gamma=0.99):
         """
-        :param step_reader: must read state from the game input and return data in a form of the named tuple,
-        so make_action can read it.
-        :param make_action: receives ai object and the results of the step_reader and this object ai object and must return action.
-        :param calculate_reward: receives the reward from the game and based on that it can return own reward
-        :param generate_history: generates the named tuple for the replay memory, should accept the action and information whether game is finished
-        :param eligibility_trace: the eligibility trace function that work with agent given to this trainer, must accept the cnn object, batch and gamme
-        :param starting_epoch: epoch that we will start training
-        :param batch_size: the size of batch that will be taken from the replay memory
+        
+        :param starting_epoch:
+        :param nb_epochs:
+        :param batch_size:
+        :param n_step:
+        :param nb_steps:
+        :param gamma:
         :return:
         """
         game = viz.DoomGame()
-        game.load_config(self.scenario_path)
+        game.load_config(self.agent.scenario_path)
         game.init()
         game.new_episode()
 
@@ -80,12 +49,12 @@ class AgentTrainer:
             reward = 0.0
             while True:
                 state = game.get_state()
-                state_data = step_reader(state=state, image_dim=self.image_dim)
-                action = make_action(ai=self.ai, state_data=state_data) if self.memory.is_buffer_full() else choice(
-                    range(0, self.nb_available_buttons))
-                game_reward = game.make_action(self.actions[action])
-                reward += calculate_reward(game_reward=game_reward)
-                history.append(generate_history(action=action, game_finished=game.is_episode_finished()))
+                state_data = self.agent.step_reader(state=state)
+                action = self.agent.make_action(state_data=state_data) if self.memory.is_buffer_full() else choice(
+                    range(0, self.agent.nb_available_buttons))
+                game_reward = game.make_action(self.agent.actions[action])
+                reward += self.agent.calculate_reward(game_reward=game_reward)
+                history.append(self.agent.generate_history(action=action, game_finished=game.is_episode_finished()))
                 if len(history) > n_step + 1:
                     history.popleft()
                 if len(history) == n_step + 1:
@@ -110,7 +79,7 @@ class AgentTrainer:
                 continue
             start = datetime.datetime.now()
             for batch in self.memory.sample_batch(batch_size):
-                image_inputs, linear_inputs, targets = eligibility_trace(cnn=self.cnn, batch=batch, gamma=gamma)
+                image_inputs, linear_inputs, targets = self.agent.eligibility_trace(batch=batch, gamma=gamma)
                 image_inputs, linear_inputs, targets = Variable(image_inputs).to(utils.DEVICE_NAME), Variable(
                     linear_inputs).to(utils.DEVICE_NAME), Variable(targets)
                 predictions = self.cnn(image_inputs, linear_inputs)
@@ -130,10 +99,10 @@ class AgentTrainer:
                 str(epoch), str(avg_reward), delta.seconds, str(datetime.datetime.now())))
             epoch += 1
             if epoch % 10 == 0:
-                model_file = "results\\cnn_doom_" + str(epoch) + ".pth"
-                score_file = "results\\scores_" + str(epoch) + ".png"
-                avg_score_file = "results\\avg_scores_" + str(epoch) + ".png"
-                memory_file = "results\\buffer_" + str(epoch) + ".pickle"
+                model_file = "results\\cnn_doom_" + self.agent.agent_identifier + "_" + str(epoch) + ".pth"
+                score_file = "results\\scores_" + self.agent.agent_identifier + "_" + str(epoch) + ".png"
+                avg_score_file = "results\\avg_scores_" + self.agent.agent_identifier + "_" + str(epoch) + ".png"
+                memory_file = "results\\buffer_" + self.agent.agent_identifier + "_" + str(epoch) + ".pickle"
                 self.memory.save_memory_buffer(memory_file)
                 print("Saving model file: {} and diagram: {}".format(model_file, score_file))
                 plt.clf()
@@ -142,7 +111,7 @@ class AgentTrainer:
                 plt.clf()
                 plt.plot(avg_history_reward, color='green')
                 plt.savefig(avg_score_file)
-                utils.save(model_file, self.cnn, self.optimizer)
+                utils.save(model_file, self.agent.cnn, self.agent.optimizer)
             if epoch == nb_epochs:
                 print("Reached last epoch, finishing...")
                 break
--- agents/agent_basic.py
+++ agents/agent_basic.py
@@ -3,26 +3,32 @@ from collections import namedtuple
 import numpy as np
 import torch
 
-import utils
 from agent import Agent
+import cnn_agent
 import image_preprocessing
+import utils
+import torch.optim as optim
+
 
 _Step = namedtuple('Step', ['state', 'action', 'reward', 'done'])
 
+
 class AgentBasic(Agent):
 
+    def __init__(self, scenario_name, agent_identifier, temperature=1.0, image_dim=80, lr=0.001):
+        super.__init__(scenario_name, agent_identifier, temperature, image_dim, lr)
+        self.cnn = cnn_agent.CNN(number_actions=self.nb_available_buttons, image_dim=image_dim)
+        self.cnn.to(utils.DEVICE_NAME)
+        self.ai = cnn_agent.AI(brain=self.cnn, body=self.softmax_body)
+        self.optimizer = optim.Adam(self.cnn.parameters(), lr=self.lr)
 
-    def __init__(self):
-        self.last_image = None
-        self.last_reward = None
-
-    def read_state(self, state, image_dim):
+    def read_state(self, state):
         buffer = state
-        self.last_image = image_preprocessing.to_grayscale_and_resize(buffer, image_dim, image_dim)
+        self.last_image = image_preprocessing.to_grayscale_and_resize(buffer, self.image_dim, self.image_dim)
         return np.array([self.last_image])
 
-    def make_action(self, ai, state_data):
-        return ai(state_data)[0][0]
+    def make_action(self, state_data):
+        return self.ai(state_data)[0][0]
 
     def calculate_reward(self, game_reward):
         self.last_reward = game_reward
@@ -31,12 +37,12 @@ class AgentBasic(Agent):
     def generate_history_record(self, action, game_finished):
         return _Step(state=self.last_image, action=action, reward=self.last_reward, done=game_finished)
 
-    def eligibility_trace(self, cnn, batch, gamma=0.99):
+    def eligibility_trace(self, batch, gamma=0.99):
         inputs = []
         targets = []
         for series in batch:
             input = torch.from_numpy(np.array([series[0].state, series[-1].state], dtype=np.float32))
-            output = cnn(input.to(utils.DEVICE_NAME))
+            output = self.cnn(input.to(utils.DEVICE_NAME))
             cumul_reward = 0.0 if series[-1].done else output[1].data.max()
             for step in reversed(series[:-1]):
                 cumul_reward = step.reward + gamma * cumul_reward
@@ -46,3 +52,7 @@ class AgentBasic(Agent):
             inputs.append(state)
             targets.append(target)
         return torch.from_numpy(np.array(inputs, dtype=np.float32)), torch.stack(targets)
+
+    def load_agent_optimizer_and_replay_memory(self, model_path, memory_path):
+        self.cnn, self.optimizer = utils.load(model_path, model_used=self.cnn, optimizer_used=self.optimizer)
+        self.memory.load_memory_buffer(memory_path)
